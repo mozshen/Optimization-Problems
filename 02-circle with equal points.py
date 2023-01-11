a2c81630f187b25dc4963b5190fa0c90919aa2c0
Mohsen Hosseini <sehosseini@mafpars.com>
2023-01-11 23:16:04 +0330

    adds constrains/objective/solver to green red circle

--- 02-circle with equal points.py
+++ 02-circle with equal points.py
@@ -23,8 +23,8 @@ def create_points(n_green, n_red, start, end):
 
 #%%
     
-n_green= np.random.randint(50, 200)
-n_red= np.random.randint(50, 200)
+n_green= np.random.randint(1, 10)
+n_red= np.random.randint(1, 10)
 start= (0, 0)
 end= (20, 20)
 
@@ -41,12 +41,12 @@ model.R = pe.Var(bounds= (0, (start[1]+end[1])/ 2))
 model.g= pe.Set(initialize= range(n_green))
 model.r= pe.Set(initialize= range(n_red))
 
-model.gf= pe.Var(model.g, within= 'Binary')
-model.rf= pe.Var(model.r, within= 'Binary')
+model.gf= pe.Var(model.g, within= pe.Binary)
+model.rf= pe.Var(model.r, within= pe.Binary)
 
 #%%
 
-def point_distance(model, i):
+def green_point_distance(model, i):
     
     x0= (start[0]+ end[0])/ 2
     y0= (start[1]+ end[1])/ 2
@@ -56,11 +56,80 @@ def point_distance(model, i):
     
     return di
 
-model.gd= pe.Param(model.g, rule= point_distance)
-model.rd= pe.Param(model.r, rule= point_distance)
+model.gd= pe.Param(model.g, rule= green_point_distance)
+
+def red_point_distance(model, i):
+    
+    x0= (start[0]+ end[0])/ 2
+    y0= (start[1]+ end[1])/ 2
+    
+    di= ((points['red'][i][0]- x0)**2+\
+        (points['red'][i][1]- y0)**2)**0.5
+    
+    return di
+
+model.rd= pe.Param(model.r, rule= red_point_distance)
+
+model.gd_positive= pe.Var(model.g)
+model.gd_negative= pe.Var(model.g)
+
+model.rd_positive= pe.Var(model.r)
+model.rd_negative= pe.Var(model.r)
 
 #%%
 
+def gdi(model, i):
+    return model.gd[i]- model.R== model.gd_positive[i]- model.gd_negative[i]
 
+model.greensdistancerule = pe.Constraint(model.g, rule=gdi)
 
+#%%
 
+def rdi(model, i):
+    return model.rd[i]- model.R== model.rd_positive[i]- model.rd_negative[i]
+
+model.redsdistancerule = pe.Constraint(model.r, rule=rdi)
+
+#%%
+
+def gdi_limit_negative(model, i):
+    return end[1]* model.gf[i]>= model.gd_negative[i]
+
+def gdi_limit_positive(model, i):
+    return end[1]* (1- model.gf[i])>= model.gd_positive[i]
+
+model.greenincircle1 = pe.Constraint(model.g, rule=gdi_limit_negative)
+model.greenincircle2 = pe.Constraint(model.g, rule=gdi_limit_positive)
+
+#%%
+
+def rdi_limit_negative(model, i):
+    return end[1]* model.rf[i]>= model.rd_negative[i]
+
+def rdi_limit_positive(model, i):
+    return end[1]* (1- model.rf[i])>= model.rd_positive[i]
+
+model.redincircle1 = pe.Constraint(model.r, rule=rdi_limit_negative)
+model.redincircle2 = pe.Constraint(model.r, rule=rdi_limit_positive)
+
+#%%
+
+def equalredgreen(model):
+    return sum([model.gf[i] for i in model.g])== sum([model.rf[i] for i in model.r])
+    
+model.equalredgreen = pe.Constraint(rule= equalredgreen)
+    
+#%%
+
+def model_objective(model):
+    return model.R
+
+model.obj = pe.Objective(rule=model_objective, sense=pe.maximize)
+
+#%%
+
+import os
+solver= pe.SolverFactory('glpk', executable= os.getcwd()+ '\\glpk-4.65\\w64\\glpsol')
+results= solver.solve(model)
+
+#%%
